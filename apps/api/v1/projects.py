f37f220dedaec19dde0e201638b6280bced89c31
renfrid <renfridfrancis@gmail.com>
2026-01-27 11:13:55 +0300

    Unsubscribe from the project

--- apps/api/v1/projects.py
+++ apps/api/v1/projects.py
@@ -19,16 +19,17 @@ from apps.projects.models import Project, ProjectMember
 class ProjectView(viewsets.ViewSet):
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsAuthenticated]
-    
+
     """API List for Project"""
+
     def get_serializer_class(self):
         if self.action in ["list", "featured"]:
             return ProjectSerializer
         return super().get_serializer_class()
-    
+
     def lists(self, request):
         """Get all projects"""
-        projects = Project.objects.filter(access='public').order_by('created_at').all()
+        projects = Project.objects.filter(access="public").order_by("created_at").all()
         serializer = ProjectSerializer(projects, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
@@ -40,9 +41,11 @@ class ProjectView(viewsets.ViewSet):
 
     def retrieve(self, request, tags=None):
         """Get projects by tags"""
-        projects = Project.objects.filter(tags__title__in=tags).order_by('created_at').all()
+        projects = (
+            Project.objects.filter(tags__title__in=tags).order_by("created_at").all()
+        )
         serializer = ProjectSerializer(projects, many=True)
-        return Response(serializer.data)    
+        return Response(serializer.data)
 
     def create(self, request):
         """Create new form definition"""
@@ -51,37 +54,100 @@ class ProjectView(viewsets.ViewSet):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
+
     def request_access(self, request):
         """Request access to project"""
-
-        code = request.data.get('code')
+        code = request.data.get("code")
         if not code:
-            return Response({"error": True, "message": "Missing project code"}, status=status.HTTP_400_BAD_REQUEST)
-    
+            return Response(
+                {"error": True, "message": "Missing project code"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
         try:
-            project = Project.objects.get(code=request.data['code'])
+            project = Project.objects.get(code=request.data["code"])
 
             # check if user is already a member
-            if ProjectMember.objects.filter(project=project, member=request.user, active=True).exists():
+            if ProjectMember.objects.filter(
+                project=project, member=request.user, active=True
+            ).exists():
                 # Return project details
-                project_data = ProjectSerializer(project).data  # or build a dict manually
-                return Response({
-                    "error": False,
-                    "message": "You are already a member of this project",
-                    "project": project_data
-                }, status=status.HTTP_200_OK)
+                project_data = ProjectSerializer(
+                    project
+                ).data  # or build a dict manually
+                return Response(
+                    {
+                        "error": False,
+                        "message": "You are already a member of this project",
+                        "project": project_data,
+                    },
+                    status=status.HTTP_200_OK,
+                )
             else:
                 # check if project is auto_join is true
                 if project.auto_join:
-                    ProjectMember.objects.create(project=project, member=request.user, active=True)
-                    return Response({"error": False, "message": "Your request has approved"},status=status.HTTP_200_OK)
+                    ProjectMember.objects.create(
+                        project=project, member=request.user, active=True
+                    )
+                    return Response(
+                        {"error": False, "message": "Your request has approved"},
+                        status=status.HTTP_200_OK,
+                    )
                 else:
-                    ProjectMember.objects.create(project=project, member=request.user, active=False)
+                    ProjectMember.objects.create(
+                        project=project, member=request.user, active=False
+                    )
 
                     # TODO: send notification to project owner
 
                     # response
-                    return Response({"error": False, "message": "Your request received, awaiting approval"},status=status.HTTP_200_OK)
+                    return Response(
+                        {
+                            "error": False,
+                            "message": "Your request received, awaiting approval",
+                        },
+                        status=status.HTTP_200_OK,
+                    )
         except:
-            return Response({"error": True, "message": "Project does not exist"},status=status.HTTP_200_OK)
+            return Response(
+                {"error": True, "message": "Project does not exist"},
+                status=status.HTTP_200_OK,
+            )
+
+    def unsubscribe(self, request, pk=None):
+        """Unsubscribe from project"""
+        code = request.data.get("code")
+        if not code:
+            return Response(
+                {"error": True, "message": "Missing project code"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        try:
+            project = Project.objects.get(code=request.data["code"])
+
+            # check if user is already a member
+            if ProjectMember.objects.filter(
+                project=project, member=request.user, active=True
+            ).exists():
+                # unsubscribe
+                ProjectMember.objects.filter(
+                    project=project, member=request.user
+                ).delete()
+                return Response(
+                    {
+                        "error": False,
+                        "message": "You have unsubscribed from this project",
+                    },
+                    status=status.HTTP_200_OK,
+                )
+            else:
+                return Response(
+                    {"error": True, "message": "You are not a member of this project"},
+                    status=status.HTTP_200_OK,
+                )
+        except:
+            return Response(
+                {"error": True, "message": "Project does not exist"},
+                status=status.HTTP_200_OK,
+            )
